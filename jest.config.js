15fa08659eea1e8dfb94598add217966aed0657b
Yasir Nadeem <m-yasir.dev@protonmail.com>
2023-02-26 19:17:58 +0500

    feat: setup test environment
    feat: setup prettier linter

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,12 @@
+// const { pathsToModuleNameMapper } = require('react-native')
+// const { compilerOptions } = require('./tsconfig.base.json')
+
 module.exports = {
   preset: 'react-native',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+  // moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  setupFilesAfterEnv: ["@testing-library/jest-native/extend-expect", "<rootDir>/jest.setup.js"],
+  // moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
+  // modulePaths: [
+  //     '<rootDir>'
+  // ],
 }
